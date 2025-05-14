6daacf392f161b909791d0dd7387b6f00d2023e9
Alexey Loik <samloik@mail.ru>
2025-05-14 17:19:25 +1000

    восстановлена работа парсера сайта сток центр - запускаем по времени на сервере линукс в тестовом режиме

---
+++ run-stok.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'Starting SH file - Parsing stok-centr.com site...'
+date
+cd /home/alex/Price-monitoring-project/Prices-Parser/ && \
+source /home/alex/Price-monitoring-project/Prices-Parser/venv.sh && \
+/home/alex/Price-monitoring-project/venv/bin/python \
+/home/alex/Price-monitoring-project/Prices-Parser/ParserStockCentrWithSession.py >> /home/alex/Price-monitoring-project/Prices-Parser/ParserStockCentrWithSession.log 2>&1
